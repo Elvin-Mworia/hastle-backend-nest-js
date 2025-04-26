fa65ad551278ea8b461986076378c02882f2d4d4
Elvin-Mworia <elvinmworia@gmail.com>
2025-04-26 14:18:48 +0300

    feat: add JobsModule to the main application module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { EmployersModule } from './employers/employers.module';
+import { JobsModule } from './jobs/jobs.module';
 
 @Module({
   imports: [
@@ -15,7 +16,8 @@ import { EmployersModule } from './employers/employers.module';
       dbName: `${process.env.DB_NAME}`,
     }),
     UsersModule,
-    EmployersModule
+    EmployersModule,
+    JobsModule
   ],
   controllers: [AppController],
   providers: [AppService],
