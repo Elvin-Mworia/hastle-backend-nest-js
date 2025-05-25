818d6be4422cbb04606144308757576f5765aac1
Elvin-Mworia <elvinmworia@gmail.com>
2025-05-25 10:43:01 +0300

    feat: enhance JwtAuthGuard with custom error handling and public route support

--- src/auth/guards/jwt-auth.guard.ts
+++ src/auth/guards/jwt-auth.guard.ts
@@ -1,14 +1,26 @@
+/* eslint-disable prettier/prettier */
 import { Injectable, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-
+import { Reflector } from '@nestjs/core';
+import { IS_PUBLIC_KEY } from '../public';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(private reflector: Reflector) {
+    super();
+  }
   /**
    * Override the canActivate method to add custom error handling
    * @param context The execution context
    * @returns A boolean or a promise resolving to a boolean
    */
   canActivate(context: ExecutionContext) {
+      const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+    if (isPublic) {
+      return true;
+    }
     return super.canActivate(context);
   }
 
